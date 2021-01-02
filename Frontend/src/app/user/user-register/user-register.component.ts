97286998d173e00019ca7f35f01f696cde6d8b9e
Yaser Marey <yasser_maree@hotmail.com>
2021-01-03 00:39:08 +0200

    Adding validation

--- Frontend/src/app/user/user-register/user-register.component.ts
+++ Frontend/src/app/user/user-register/user-register.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, Validator } from '@angular/forms';
 
 @Component({
   selector: 'app-user-register',
   templateUrl: './user-register.component.html',
   styleUrls: ['./user-register.component.css']
 })
+
 export class UserRegisterComponent implements OnInit {
 
   registerationForm: FormGroup;
@@ -15,7 +16,36 @@ export class UserRegisterComponent implements OnInit {
   ngOnInit() {
 
     this.registerationForm = new FormGroup({
-      userName: new FormControl('Yaser', Validators.required) });
+      userName: new FormControl('Yaser', Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+      confirmPassword: new FormControl(null, [Validators.required]),
+      mobile: new FormControl(null, [Validators.required, Validators.maxLength(11)])
+     }, this.passwordMathcingValidator);
+  }
+
+  passwordMathcingValidator(fg: FormGroup): Validators {
+    return fg.get('password').value === fg.get('confirmPassword').value ? null : { notmatched: true }
+  }
+
+  get userName() {
+    return this.registerationForm.get('userName') as FormControl
+  }
+
+  get email() {
+    return this.registerationForm.get('email') as FormControl
+  }
+
+  get password() {
+    return this.registerationForm.get('password') as FormControl
+  }
+
+  get confirmPassword() {
+    return this.registerationForm.get('confirmPassword') as FormControl
+  }
+
+  get mobile() {
+    return this.registerationForm.get('mobile') as FormControl
   }
 
   onSubmti() {
